0dc114048cb1fc55a946e9de85ffc229e10ac02f
Adriano Alberti <al.adriano@hotmail.com>
2025-03-21 12:19:05 +0100

    Configuración inicial

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application {
 
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/hellofx.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
