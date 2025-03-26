c6f84eea74082fca6b5ddf96e892adbc72f7d9dc
Hugo <131997812+ShyGuy-del@users.noreply.github.com>
2025-03-26 17:43:43 +0100

    Añadida la funcionalidad del metodo comprobar Registro y el metodo comprobar Login(hugo)
    Se añadirá la funcionalidad completa del método comprobar registro y login cuando tengamos la conexion con la base de datos

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -9,36 +9,95 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
-
+	int contadorError = 1;
+	int contadorErrorLogin = 1;
 	public Stage stage;
-	
-    @FXML
-    private Label ErrorNombre;
 
-    @FXML
-    private Label ErrorPass;
+	@FXML
+	private Label ErrorNombre;
 
-    @FXML
-    private Button Loginbtn;
-    @FXML
-    private Button Registrarse;
-    @FXML
-    private PasswordField Password;
+	@FXML
+	private Label ErrorPass;
 
-    @FXML
-    private TextField TfNombre;
+	@FXML
+	private Button Loginbtn;
+	@FXML
+	private Button Registrarse;
+	@FXML
+	private PasswordField Password;
 
-    @FXML
-    void comprobarLogin(ActionEvent event) {
+	@FXML
+	private TextField TfNombre;
 
-    }
-   
-    @FXML
-    void comprobarRegistro(ActionEvent event) {
-    	System.out.println("Te has registrado");
-    }
+	@FXML
+	void comprobarLogin(ActionEvent event) {
+		boolean mover = false;
+
+		ErrorPass.setLayoutX(225);
+		ErrorNombre.setLayoutX(225);
+		if (TfNombre.getText().isEmpty()) {
+			System.out.println("no se ha podido llevar a cabo el registro");
+			ErrorNombre.setVisible(true);
+			ErrorNombre.setText("Nombre vacío");
+			mover = true;
+		} else if (TfNombre.getText() == "") {
+		} else {
+			System.out.println("Te has registrado con éxito");
+			ErrorNombre.setVisible(false);
+		}
+		if (Password.getText().isEmpty()) {
+			System.out.println("no se ha podido llevar a cabo el registro");
+			if (mover) {
+				double Y = ErrorPass.getLayoutY();
+				if (contadorError == 1) {
+					ErrorPass.setLayoutY(Y + 25);
+					contadorError--;
+				}
+			}
+			ErrorPass.setVisible(true);
+			ErrorPass.setText("Contraseña vacía");
+		} else if (Password.getText() == "") {
+		} else {
+			System.out.println("Te has registrado con éxito");
+			ErrorPass.setVisible(false);
+		}
+
+	}
+
+	@FXML
+	void comprobarRegistro(ActionEvent event) {
+		boolean mover = false;
+
+		ErrorPass.setLayoutX(225);
+		ErrorNombre.setLayoutX(225);
+		if (TfNombre.getText().isEmpty()) {
+			System.out.println("no se ha podido llevar a cabo el registro");
+			ErrorNombre.setVisible(true);
+			ErrorNombre.setText("Nombre vacío");
+			mover = true;
+		} else {
+			System.out.println("Te has registrado con éxito");
+			ErrorNombre.setVisible(false);
+		}
+		if (Password.getText().isEmpty()) {
+			System.out.println("no se ha podido llevar a cabo el registro");
+			if (mover) {
+				double Y = ErrorPass.getLayoutY();
+				if (contadorError == 1) {
+					ErrorPass.setLayoutY(Y + 25);
+					contadorError--;
+				}
+			}
+			ErrorPass.setVisible(true);
+			ErrorPass.setText("Contraseña vacía");
+		} else {
+			System.out.println("Te has registrado con éxito");
+			ErrorPass.setVisible(false);
+		}
+
+	}
 
 	public void setStage(Stage primaryStage) {
-		stage = primaryStage;		
+		stage = primaryStage;
 	}
 }
