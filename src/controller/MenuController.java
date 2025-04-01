1f0830796157cf668c39ed0c5c586a596aee1458
PedroCaballero23 <pedro.s436646@cesurformacion.com>
2025-04-01 12:48:50 +0200

    Pantalla menu

---
+++ src/controller/MenuController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MenuController {
+
+    @FXML
+    private ImageView ImgBolsa;
+
+    @FXML
+    private ImageView ImgCentroPokemon;
+
+    @FXML
+    private ImageView ImgCombate;
+
+    @FXML
+    private ImageView ImgEquipo;
+
+    @FXML
+    private ImageView ImgFondo;
+
+    @FXML
+    private Label LblJugador;
+
+    @FXML
+    private ImageView imgCaptura;
+
+    @FXML
+    private ImageView imgCrianza;
+
+    @FXML
+    private Label lblDinero;
+
+    @FXML
+    private Label lblDinero1;
+
+    @FXML
+    private Label lblPesetas;
+
+}
