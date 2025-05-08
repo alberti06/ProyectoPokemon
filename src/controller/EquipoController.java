558829ab4f681b36caeee745e4742fa095884521
PedroCaballero23 <pedro.s436646@cesurformacion.com>
2025-05-08 18:04:55 +0200

    modificacion pantalla equipo

--- src/controller/EquipoController.java
+++ src/controller/EquipoController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.awt.Label;
 import java.io.File;
 import java.io.IOException;
 
@@ -7,9 +8,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import model.Entrenador;
 
@@ -20,8 +23,83 @@ public class EquipoController {
 	private Entrenador entrenador;
 	private LoginController loginController;
 
+	@FXML
+    private ProgressBar barPokemon1;
+
+    @FXML
+    private ProgressBar barPokemon2;
+
+    @FXML
+    private ProgressBar barPokemon3;
+
+    @FXML
+    private ProgressBar barPokemon4;
+
+    @FXML
+    private ProgressBar barPokemon5;
+
+    @FXML
+    private ProgressBar barPokemon6;
+
     @FXML
     private ImageView btnSalir;
+
+    @FXML
+    private AnchorPane imgFondo;
+
+    @FXML
+    private ImageView imgPokemon1;
+
+    @FXML
+    private ImageView imgPokemon2;
+
+    @FXML
+    private ImageView imgPokemon3;
+
+    @FXML
+    private ImageView imgPokemon4;
+
+    @FXML
+    private ImageView imgPokemon5;
+
+    @FXML
+    private ImageView imgPokemon6;
+
+    @FXML
+    private Label lblNombre1;
+
+    @FXML
+    private Label lblNombre2;
+
+    @FXML
+    private Label lblNombre3;
+
+    @FXML
+    private Label lblNombre4;
+
+    @FXML
+    private Label lblNombre5;
+
+    @FXML
+    private Label lblNombre6;
+
+    @FXML
+    private Label lblPS2;
+
+    @FXML
+    private Label lblPS3;
+
+    @FXML
+    private Label lblPS4;
+
+    @FXML
+    private Label lblPS5;
+
+    @FXML
+    private Label lblPS6;
+
+    @FXML
+    private Label lblPs1;
 
     @FXML
     void salirMenupoke(MouseEvent event) {
